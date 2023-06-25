1f9834b49463a1bff24e4b1cdb6f9ab00f02a13d
adhito909 <adhito909@gmail.com>
2023-06-25 16:55:00 +0700

    Create Spring JPA2 and H2 database configuration and depedencies

--- src/main/java/com/learningspringbootms/mscurrencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/learningspringbootms/mscurrencyexchangeservice/controller/CurrencyExchangeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.learningspringbootms.mscurrencyexchangeservice.CurrencyExchange;
+import com.learningspringbootms.mscurrencyexchangeservice.entity.CurrencyExchange;
 
 @RestController
 public class CurrencyExchangeController {
