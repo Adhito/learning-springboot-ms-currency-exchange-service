3812cebb22ae7c7358001a6e27738c428458a2bb
adhito909 <adhito909@gmail.com>
2023-06-25 15:16:31 +0700

    Create initial base ms-currency-exchange-service source code

---
+++ src/main/java/com/learningspringbootms/mscurrencyexchangeservice/MsCurrencyExchangeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.learningspringbootms.mscurrencyexchangeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCurrencyExchangeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCurrencyExchangeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learningspringbootms/mscurrencyexchangeservice/MsCurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningspringbootms.mscurrencyexchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCurrencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
